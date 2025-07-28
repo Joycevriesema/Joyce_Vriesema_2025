cad53c2dffb50381f3498ecd1c99b98c3cdd6246
Joyce Vriesema <j.vriesema.1@student.rug.nl>
2025-07-28 15:48:22 +0200

    calculated bird counts per meter shoreline corrected for tree volume

--- Bird_analysis.R
+++ Bird_analysis.R
@@ -92,6 +92,96 @@ ggplot(data_bird, aes(x = distance_to_river_mouth, y = total_count, fill = river
     plot.margin = margin(10, 10, 10, 10)
   )
 
+
+#### bird counts corrected for visible tree volume along the shoreline
+# join data_bird with data_trees, keep 94 observations of the bird df
+data_trees <- read.csv("data_trees.csv")
+data_bird_trees <- left_join(data_bird, data_trees, by = c("transect_ID", "habitat_detailed", "habitat_main", "habitat_detailed_2", "river", "distance_to_river_mouth"))
+
+# calculate the number of birds per meter shoreline counted
+# for papyrus this is the total number of bird counts / 500 m shoreline (the whole transect length)
+# for tree this is the total number of bird counts / total visible circumfence
+birds_meter_shoreline <- data_bird_trees |>
+  mutate(
+    birds_per_meter = case_when(
+      habitat_main == "Papyrus" ~ total_count / 500,
+      habitat_main == "Trees" ~ total_count / total_visible_circumference,
+      TRUE ~ NA_real_  # catch-all for other habitat types if any
+    ),
+    birds_per_100m = birds_per_meter * 100
+  )
+
+
+# plot bird counts per tree volume
+ggplot(birds_meter_shoreline |> dplyr:: filter(habitat_main == "Trees"), aes(x = total_visible_tree_volume, y = total_count)) +
+  geom_point(size = 2, color = "#0072B2")+
+  geom_smooth(method = "lm", se = TRUE, color = "black", linetype = "solid") +
+  #geom_text_repel(aes(label = transect_ID), size = 4, fontface = "bold") +
+  labs(
+    x = "Visible tree volume (m³)",
+    y = "Total bird count"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(color = "black")
+  )
+
+# plot with symbols for the different habitat combinations
+ggplot(birds_meter_shoreline|> dplyr:: filter(habitat_main == "Trees"), aes(x = total_visible_tree_volume, y = total_count)) +
+  geom_point(aes(color = habitat_detailed, shape = habitat_detailed), size = 3) +
+  geom_smooth(method = "lm", se = TRUE, color = "black", linetype = "solid") +
+  labs(
+    x = "Visible tree volume (m³)",
+    y = "Total bird count",
+    color = "Habitat type",
+    shape = "Habitat type"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.title = element_text(face = "bold"),
+    axis.text = element_text(color = "black"),
+    legend.position = "right"
+  )
+
+
+# plot bird counts per 100 meter shoreline
+ggplot(birds_meter_shoreline, aes(x = distance_to_river_mouth, y = birds_per_100m, fill = river)) +
+  geom_boxplot(
+    color = "black",     
+    outlier.shape = 21,    
+    outlier.fill = "black",
+    outlier.color = "black",
+    outlier.size = 2
+  ) +
+  facet_grid(~ habitat_main) +
+  #scale_y_log10() +
+  scale_fill_manual(values = c(
+    "Mbalageti" = "#0072B2",
+    "Robana" = "#56B4E9"
+  ))+
+  labs(
+    x = "Distance to river mouth",
+    y = "Count / 100 meter shoreline",
+    fill = "River"
+  ) +
+  theme_minimal(base_size = 14) +
+  theme(
+    axis.title = element_text(size = 13, face="bold"),
+    axis.title.x = element_text(size = 13, face = "bold", margin = margin(t = 10)),
+    axis.text.x = element_text(size = 11, face = "bold", color="black"), 
+    axis.text = element_text(size = 11),
+    legend.position = "right",
+    legend.title = element_text(size = 13, face = "bold"),
+    strip.text = element_text(size = 13, face = "bold"),
+    panel.grid.major.x = element_line(color = "grey80", linetype = "solid"),
+    panel.grid.major.y = element_line(color = "grey80", linetype = "solid"),
+    panel.grid.minor = element_blank(),
+    panel.spacing = unit(1.2, "lines"),
+    panel.border = element_rect(color = "black", size = 1, fill = NA),
+    plot.margin = margin(10, 10, 10, 10)
+  )
+
 #### model analysis papyrus transects ####
 # use Poisson regression model to test whether the total count of Pied kingfishers differs significantly between transects within each habitat type
 # due to uneven number of the paired transect design it is only possible to compare within a habitat and not between habitats papyrus: n=6, trees: n=8
