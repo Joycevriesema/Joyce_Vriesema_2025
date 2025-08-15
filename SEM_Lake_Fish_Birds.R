dd59ce60feb20b55f5d84d9f2b2e18c29c2c3939
Joyce Vriesema <j.vriesema.1@student.rug.nl>
2025-08-15 17:19:45 +0200

    - merged datasets into one big dataframe

--- SEM_Lake_Fish_Birds.R
+++ SEM_Lake_Fish_Birds.R
@@ -21,21 +21,43 @@ fish_data <- read.csv("fish_data.csv") |>
 PCA_data <- read.csv("PCA_data.csv") |>
   mutate(date = as.Date(date))
 
+# fish_data and data_bird first need to be merged with corresponding transect_run_ID from data transect
+# load data_transect and filter out old data (5-Feb-2025 & 8-Feb-2025)
+data_transect <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRCwiQGeumB9AuvRjnobaDJLq76NWyPQrvnPdvP58Qxv5SGMt4LMKjxMQMREGnYdoIkO1oCfTOcqp1Z/pub?gid=1366617186&single=true&output=csv") %>%
+  dplyr::filter(date != "5-Feb-2025" & date != "8-Feb-2025") |>
+  dplyr::select(transect_run_ID,run_ID, transect_ID, date) |>
+  mutate(date=as.Date(date, format= "%d-%b-%Y"))
+
+
+# merge data_bird and data_transect
+data_bird<- data_bird %>%
+  dplyr::left_join(data_transect, by = c("transect_ID", "date"))
+
+# merge fish_data and data_transect
+fish_data <- fish_data %>%
+  dplyr::left_join(data_transect, by = c("transect_ID", "date"))
+
 # join dataframes
+# joinn birds with fish according to transect_runID
 joined_data <- data_bird |>
-  left_join(fish_data, by = c("transect_ID", "river", "habitat_detailed", 
-                              "habitat_detailed_2", "distance_to_river_mouth", 
-                              "habitat_main", "date"), suffix = c(".bird", ".fish")) |>
-  left_join(PCA_data, by = c("transect_ID", "river", "habitat_detailed", 
-                             "habitat_detailed_2", "distance_to_river_mouth", 
-                             "habitat_main", "date"), suffix = c("", ".pca"))
+  left_join(
+    fish_data |>
+      dplyr::select(-one_of(setdiff(intersect(names(data_bird), names(fish_data)), "transect_run_ID"))),
+    by = "transect_run_ID"
+  ) %>%
+  { 
+    current_names <- names(.)
+    left_join(
+      .,
+      PCA_data |>
+        dplyr::select(-one_of(setdiff(intersect(current_names, names(PCA_data)), "transect_run_ID"))),
+      by = "transect_run_ID"
+    )
+  }
 
-# PCA data has only 89 observations and therefore there are NA's
-missing_date_transect <- joined_data %>%
-  filter(if_any(everything(), is.na)) %>%
-  distinct(date, transect_ID)
 
-print(missing_date_transect)
-# mostly for 13 feb and 10 apr
 
-# variables of interest in this dataset are:
+
+
+
+
