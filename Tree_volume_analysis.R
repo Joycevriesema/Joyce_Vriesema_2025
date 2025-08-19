e7fe023cf4a833089c5b4239f415efa9337ef51b
Joyce Vriesema <j.vriesema.1@student.rug.nl>
2025-08-19 22:41:08 +0200

    runned script for updated transects

--- Tree_volume_analysis.R
+++ Tree_volume_analysis.R
@@ -7,14 +7,14 @@ library(emmeans)    # pairwise comparison
 library(patchwork)  # combine multiple ggplot2 plots into one layout 
 
 # load tree data
-data_trees <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRCwiQGeumB9AuvRjnobaDJLq76NWyPQrvnPdvP58Qxv5SGMt4LMKjxMQMREGnYdoIkO1oCfTOcqp1Z/pub?gid=1205571162&single=true&output=csv") |>
+data_trees <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRCwiQGeumB9AuvRjnobaDJLq76NWyPQrvnPdvP58Qxv5SGMt4LMKjxMQMREGnYdoIkO1oCfTOcqp1Z/pub?gid=1205571162&single=true&output=csv")|>
   mutate(date=as.Date(date, format= "%d-%b-%Y"),
          habitat_detailed = case_when(
            transect_ID %in% c("pap_1", "pap_2") ~ "Papyrus Mbalageti Mouth",
            transect_ID %in% c("pap_3", "pap_4") ~ "Papyrus Robana Mouth",
-           transect_ID %in% c("pap_5", "pap_6") ~ "Papyrus Robana Far",
+           transect_ID %in% c("pap_5", "pap_6") ~ "Papyrus Robana Mid",
            transect_ID %in% c("tree_1", "tree_2") ~ "Tree Mbalageti Mid",
-           transect_ID %in% c("tree_3", "tree_4") ~ "Tree Robana Mid",
+           transect_ID %in% c("tree_3", "tree_4") ~ "Tree Mbalageti Far",
            transect_ID %in% c("tree_5", "tree_6") ~ "Tree Robana Mouth",
            transect_ID %in% c("tree_7", "tree_8") ~ "Tree Robana Far",
            TRUE ~ NA_character_),
@@ -22,24 +22,24 @@ data_trees <- read.csv("https://docs.google.com/spreadsheets/d/e/2PACX-1vRCwiQGe
            transect_ID %in% c("pap_1", "pap_2","pap_3", "pap_4","pap_5", "pap_6" ) ~ "Papyrus",
            transect_ID %in% c("tree_1", "tree_2","tree_3", "tree_4","tree_5", "tree_6","tree_7", "tree_8") ~ "Trees",
            TRUE ~ NA_character_),
-         distance_to_river_mouth= case_when(
+         distance_to_river_mouth = case_when(
            transect_ID %in% c("pap_1", "pap_2", "pap_3", "pap_4", "tree_5", "tree_6") ~ "Mouth",
-           transect_ID %in% c("tree_1", "tree_2", "tree_3", "tree_4") ~ "Mid",
-           transect_ID %in% c("pap_5", "pap_6", "tree_7", "tree_8") ~ "Far"
+           transect_ID %in% c("tree_1", "tree_2","pap_5", "pap_6" ) ~ "Mid",
+           transect_ID %in% c("tree_7", "tree_8","tree_3", "tree_4") ~ "Far"
          ),
          distance_to_river_mouth= factor(distance_to_river_mouth, levels= c("Mouth", "Mid", "Far")),
-         habitat_detailed_2= case_when(
+         habitat_detailed2= case_when(
            transect_ID %in% c("pap_1", "pap_2") ~ "Mbalageti Mouth",
            transect_ID %in% c("pap_3", "pap_4","tree_5", "tree_6") ~ "Robana Mouth",
-           transect_ID %in% c("pap_5", "pap_6","tree_7", "tree_8") ~ "Robana Far",
+           transect_ID %in% c("tree_7", "tree_8") ~ "Robana Far",
+           transect_ID %in% c("pap_5", "pap_6") ~ "Robana Mid",
            transect_ID %in% c("tree_1", "tree_2") ~ "Mbalageti Mid",
-           transect_ID %in% c("tree_3", "tree_4") ~ "Robana Mid",
+           transect_ID %in% c("tree_3", "tree_4") ~ "Mbalageti Far",
            TRUE ~ NA_character_),
          river= case_when(
-           transect_ID %in% c("pap_1", "pap_2", "tree_1", "tree_2") ~ "Mbalageti",
-           transect_ID %in% c("pap_3", "pap_4", "pap_5", "pap_6", "tree_3", "tree_4","tree_5", "tree_6", "tree_7", "tree_8") ~ "Robana"
-         )) |>
-  group_by(transect_ID,river, habitat_detailed, habitat_detailed_2, distance_to_river_mouth, habitat_main )|>
+           transect_ID %in% c("pap_1", "pap_2", "tree_1", "tree_2","tree_3", "tree_4") ~ "Mbalageti",
+           transect_ID %in% c("pap_3", "pap_4", "pap_5", "pap_6","tree_5", "tree_6", "tree_7", "tree_8") ~ "Robana"))|>
+  group_by(transect_ID,river, habitat_detailed, habitat_detailed2, distance_to_river_mouth, habitat_main )|>
   filter(habitat_main!= "Papyrus")
 
 # calculate tree volume (as ellyptical cylinder) for every transect
